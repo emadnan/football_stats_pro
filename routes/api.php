5e44729c0981342354b42db4e88399d0d8ebd711
emadnan <muhammad.adnan@spmo.uol.edu.pk>
2023-07-20 17:06:11 +0500

    Register, login and logout routes are created

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+Route::group(['middleware' => 'auth:sanctum'], function(){
+    //All secure URL's
+    Route::post('/logout', [UserController::class,'logout']);
+
+    });
+
+
+Route::post("login",[UserController::class,'index']);
+Route::post('/register', [UserController::class,'register']);
