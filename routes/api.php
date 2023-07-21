64d82bd4f8b1ee220e036a822a264bf3095ff4cd
Jahanzaib Baig <133633113+Jahanzaibbaig36@users.noreply.github.com>
2023-07-21 15:31:12 +0500

    API Route added for Live Status Controller

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\LiveStatsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,13 +19,13 @@ use App\Http\Controllers\UserController;
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
+
 Route::group(['middleware' => 'auth:sanctum'], function(){
     //All secure URL's
     Route::post('/logout', [UserController::class,'logout']);
+});
 
-    });
-
-
+Route::get('/live-stats', [LiveStatsController::class, 'getLiveStats']);
 Route::post("login",[UserController::class,'index']);
 Route::post('/register', [UserController::class,'register']);
 Route::get("users",[UserController::class,'getUsers']);
